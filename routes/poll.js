a9d6ac7ad3cb8213b3b3f5f0679f282e28e06b6c
harshit kohli <harshitkohli1997@github.com>
2018-06-23 16:39:54 +0530

    try

--- routes/poll.js
+++ routes/poll.js
@@ -1,10 +1,39 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/', (req,res) => {
-    res.render('poll');
+const Vote = require('../models/Vote');
+
+const Pusher = require('pusher');
+
+const keys = require('../config/keys');
+
+var pusher = new Pusher({
+  appId: keys.pusherAppId,
+  key: keys.pusherKey,
+  secret: keys.pusherSecret,
+  cluster: keys.pusherCluster,
+  encrypted: keys.pusherEncrypted
 });
 
+router.get('/', (req, res) => {
+  Vote.find().then(votes => res.json({ success: true, votes: votes }));
+});
+
+router.post('/', (req, res) => {
+  const newVote = {
+    movie: req.body.movie,
+    points: 1
+  };
+
+  new Vote(newVote).save().then(vote => {
+    pusher.trigger('movie-poll', 'movie-vote', {
+      points: parseInt(vote.points),
+      movie: vote.movie
+    });
+
+    return res.json({ success: true, message: 'Thank you for voting' });
+  });
+});
 
 module.exports = router;
